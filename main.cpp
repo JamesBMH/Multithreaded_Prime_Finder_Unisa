5a2a8d07d3a6c1d8281551ff9d021c63d5cc53ae
JamesBMH <jameshertzog3@gmail.com>
2025-02-06 15:30:47 +0200

    Added readme and project files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//13413120 JBM HHERTZOG Question 4
+#include "gui.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Gui w;
+    w.show();
+    return a.exec();
+}
